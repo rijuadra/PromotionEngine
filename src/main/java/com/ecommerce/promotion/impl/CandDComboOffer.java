1e44b39b330a84d6384001057689993c8405c9b1
Saikat Das <rijuadra@gmail.com>
2020-09-19 13:01:41 +0530

    C & D combo offer code added

--- src/main/java/com/ecommerce/promotion/impl/CandDComboOffer.java
+++ src/main/java/com/ecommerce/promotion/impl/CandDComboOffer.java
@@ -1,12 +1,19 @@
 package com.ecommerce.promotion.impl;
 
 import com.ecommerce.promotion.IPromotion;
-
+import static com.ecommerce.bo.ProductCatalog.*;
 import java.util.HashMap;
 
 public class CandDComboOffer implements IPromotion {
     @Override
     public long calculatePriceWithPromotion(HashMap<String, Integer> checkoutProductList) {
-        return 0;
+        int quantityOfC = checkoutProductList.get("C")==null?0:checkoutProductList.get("C");
+        int quantityOfD = checkoutProductList.get("D")==null?0:checkoutProductList.get("D");
+        int price=0;
+        if(quantityOfC>quantityOfD)
+            price=quantityOfD*30+(quantityOfC-quantityOfD)* productList.get("C");
+        else
+            price=quantityOfC*30+(quantityOfD-quantityOfC)* productList.get("D");
+        return price;
     }
 }
