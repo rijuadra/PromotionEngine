eaa3f06e8d2478b49825994786768377c4859352
Saikat Das <rijuadra@gmail.com>
2020-09-19 12:49:52 +0530

    2B Discount Promotion calculation code added

--- src/main/java/com/ecommerce/promotion/impl/TwoBDiscountOffer.java
+++ src/main/java/com/ecommerce/promotion/impl/TwoBDiscountOffer.java
@@ -1,12 +1,14 @@
 package com.ecommerce.promotion.impl;
 
 import com.ecommerce.promotion.IPromotion;
-
+import static com.ecommerce.bo.ProductCatalog.*;
 import java.util.HashMap;
 
 public class TwoBDiscountOffer implements IPromotion {
     @Override
     public long calculatePriceWithPromotion(HashMap<String, Integer> checkoutProductList) {
-        return 0;
+        int quantityOfB = checkoutProductList.get("B")==null?0:checkoutProductList.get("B");
+        int price = quantityOfB / 2 * 45 + quantityOfB % 2 * productList.get("B");
+        return price;
     }
 }
